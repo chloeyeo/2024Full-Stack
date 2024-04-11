c37d0b2fd7d8264b15e844bc554e0049d29b422c
Chloe Yeo <yeochloe01@gmail.com>
2024-04-11 15:30:52 +0900

    navbar created

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/css/tailwindStyle.scss";
+import React from "react"; // to use jsx
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import { Routes, Route, NavLink, Outlet } from "react-router-dom";
+import NavBar from "./layout/NavBar";
+
+function Layout() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ front/src/layout/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const routes = [
+    { to: "/", name: "home" },
+    { to: "/login", name: "login" },
+    { to: "/register", name: "register" },
+  ];
+  return (
+    <div className="w-full shadow-md">
+      <div className="container m-auto bg-blue-100 flex justify-between">
+        <h1 className="font-semibold p-4">COMPANY</h1>
+        <ul className="flex">
+          {routes.map((route, i) => {
+            return (
+              <li key={`menuItem-${i}`}>
+                <Link
+                  className="h-full flex px-4 justify-center items-center"
+                  to={route.to}
+                >
+                  {route.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
