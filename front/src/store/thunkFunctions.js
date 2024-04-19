6cfd78a2351526852e74787d96750ce146059b58
Chloe Yeo <yeochloe01@gmail.com>
2024-04-19 12:35:40 +0900

    update

--- front/src/store/thunkFunctions.js
+++ front/src/store/thunkFunctions.js
@@ -5,9 +5,11 @@ export const loginUser = createAsyncThunk("user/loginuser", async (body) => {
   try {
     const response = await axiosInstance.post("/user/login", body);
     console.log("login");
+    console.log("login response.data", response.data);
     return response.data;
   } catch (error) {
-    console.error(error.message);
+    console.log("login response error");
+    return console.error(error.message);
   }
 });
 
@@ -15,9 +17,12 @@ export const authUser = createAsyncThunk("user/authuser", async () => {
   try {
     console.log("about to send get request to auth");
     const response = await axiosInstance.get("/user/auth");
+    console.log("authorise");
+    console.log("auth response.data", response.data);
     return response.data;
   } catch (error) {
-    console.error(error.message);
+    console.log("auth response error");
+    return console.error(error.message);
   }
 });
 
@@ -25,8 +30,10 @@ export const logoutUser = createAsyncThunk("user/logoutuser", async () => {
   try {
     const response = await axiosInstance.post("/user/logout");
     console.log("logout");
+    console.log("logout response.data", response.data);
     return response.data;
   } catch (error) {
-    console.error(error.message);
+    console.log("logout response error");
+    return console.error(error.message);
   }
 });
