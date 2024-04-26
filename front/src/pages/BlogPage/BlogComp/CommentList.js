4c4e072dac3dec1ec59cca79e9ef73abbcea4aec
Chloe Yeo <yeochloe01@gmail.com>
2024-04-26 11:13:51 +0900

    updated deleteComment so that only the comment author can delete their comment

--- front/src/pages/BlogPage/BlogComp/CommentList.js
+++ front/src/pages/BlogPage/BlogComp/CommentList.js
@@ -1,11 +1,17 @@
+import { useSelector } from "react-redux";
+
 const CommentList = ({ comment, deleteComment }) => {
+  const user = useSelector((state) => state.user?.userData);
+  const loggedInUserId = user.id;
+  const commentAuthorId = comment.user._id;
+  const isCommentAuthor = loggedInUserId === commentAuthorId;
   const handleDelete = () => {
     deleteComment(comment._id);
   };
   return (
     <div>
       {comment.content} / By {comment.user.name}
-      <button onClick={handleDelete}>delete</button>
+      {isCommentAuthor && <button onClick={handleDelete}>delete</button>}
     </div>
   );
 };
